3e242afdd147bfd1bf84782ae5446c0049393213
jamesdbruner <jamesdbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-18 04:52:14 +0000

    Fixed more weird spacing that was going on only with the .php files.  Fairly sure it has something to do with the text editor I was using.
    git-svn-id: https://plugins.svn.wordpress.org/vegas-fullscreen-background-slider/tags/2.0@876969 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- methods/vegas_cpt.php
+++ methods/vegas_cpt.php
@@ -1,28 +1,28 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-	register_post_type( 'vegas',
-	array(
-	'menu_icon' => 'dashicons-format-gallery',
-	'labels' => array(
-	'name' => __( 'Vegas Slideshows' ),
-	'singular_name' => __( 'Slideshow' ),
-	'menu_name' => __( 'WP Vegas' ),
-	'all_items' => __( 'View Slideshows' ),
-	'add_new' => _x('Add Slideshow', 'Slideshow'),
-	'add_new_item' => __('New Slideshow'),
-	'edit_item' => __('Edit Slideshow'),
-	'new_item' => __('New Slideshow'),
-	'view_item' => __('View Slideshow'),
-	'search_items' => __('Search Your Slideshows'),
-	'not_found' =>  __('Nothing found'),
-	'not_found_in_trash' => __('Nothing found in Trash'),
-	),
-	'public' => false,
-	'has_archive' => true,
-	'publicly_queryable' => true,
-	'show_ui' => true,
-	'query_var' => true,
-	'rewrite' => array('slug' => 'vegas_slideshows'),
-	'capability_type' => 'post',
-	'supports' => array('title'),
-	'taxonomies' => array( 'slideshow'))); ?>
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+	register_post_type( 'vegas',
+	array(
+	'menu_icon' => 'dashicons-format-gallery',
+	'labels' => array(
+	'name' => __( 'Vegas Slideshows' ),
+	'singular_name' => __( 'Slideshow' ),
+	'menu_name' => __( 'WP Vegas' ),
+	'all_items' => __( 'View Slideshows' ),
+	'add_new' => _x('Add Slideshow', 'Slideshow'),
+	'add_new_item' => __('New Slideshow'),
+	'edit_item' => __('Edit Slideshow'),
+	'new_item' => __('New Slideshow'),
+	'view_item' => __('View Slideshow'),
+	'search_items' => __('Search Your Slideshows'),
+	'not_found' =>  __('Nothing found'),
+	'not_found_in_trash' => __('Nothing found in Trash'),
+	),
+	'public' => false,
+	'has_archive' => true,
+	'publicly_queryable' => true,
+	'show_ui' => true,
+	'query_var' => true,
+	'rewrite' => array('slug' => 'vegas_slideshows'),
+	'capability_type' => 'post',
+	'supports' => array('title'),
+	'taxonomies' => array( 'slideshow')));
